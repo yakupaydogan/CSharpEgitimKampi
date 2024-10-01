c8bcc1d8e87db00359cf25b26358b058aae0acca
Yakup Aydoğan <1yakupaydogan@gmail.com>
2024-10-01 14:51:14 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region YazdirmaKomutlari
+            //Console.Write("Hello World");
+            //Console.WriteLine("Selam");
+            #endregion
+            #region Degiskenler
+            //string
+            //degisken_turu degisken_adi
+            //string name;
+            //name = "Yakup";
+            //Console.WriteLine(name);
+            // string customerName; //camelCase
+
+            #endregion
+            
+
+            Console.Read();
+        }
+    }
+}
+
+
+//Yazdırma komutları
+//IntelliSense*, bir dizi özelliği içeren bir kod tamamlama yardımıdır.
+//variables
---
+++ 02_Variables/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Double Variables
+            //double number;
+            //number = 4.85;
+            //Console.WriteLine(number);
+            
+
+            //Console.WriteLine("***** Fiyat Listesi *****");
+            //Console.WriteLine();
+
+            //double applePrice, orangePrice, strawberryPrice, patatoPrice, tomatoPrice;
+            //applePrice = 14.85;
+            //orangePrice = 20.95;
+            //strawberryPrice = 45;
+            //patatoPrice = 9.74;
+            //tomatoPrice = 6.88;
+
+            //Console.WriteLine("---- Elma Birim Fiyatı:" + applePrice + " TL");
+            //Console.WriteLine("---- Portakal Birim Fiyatı:" + orangePrice + " TL");
+            //Console.WriteLine("---- Çilek Birim Fiyatı:" + strawberryPrice + " TL");
+            //Console.WriteLine("---- Patates Birim Fiyatı:" + patatoPrice + " TL");
+            //Console.WriteLine("---- Domates Birim Fiyatı:" + tomatoPrice + " TL");
+
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            //double appleGram, orangeGram, strawGram, patatoGram, tomatoGram;
+            //appleGram = 1.245;
+            //orangeGram = 2.650;
+            //strawGram = 7.50;
+            //patatoGram = 4.859;
+            //tomatoGram = 3.745;
+
+            //double appleTotalPrice = appleGram * appleGram;
+            //double orangeTotalPrice = orangeGram * orangeGram;
+            //double strawberryTotalPrice = strawberryPrice * strawberryPrice;
+            //double potatoTotalPrice = patatoGram * patatoPrice;
+            //double tomatoTotalPrice = tomatoGram * tomatoPrice;
+
+
+            //Console.WriteLine("Alınan Ürün: Elma - " + "Birim Fiyat: " + applePrice + " - Gramaj: " + appleGram + " - Toplam Tutar: " + appleTotalPrice);
+            //Console.WriteLine("Alınan Ürün: Portakal - " + "Birim Fiyat: " + orangePrice + " - Gramaj: " + orangeGram + " - Toplam Tutar: " + orangeTotalPrice);
+            //Console.WriteLine("Alınan Ürün: Çilek - " + "Birim Fiyat: " + strawberryPrice + " - Gramaj: " + strawGram + " - Toplam Tutar: " + strawberryTotalPrice);
+            //Console.WriteLine("Alınan Ürün: Patates - " + "Birim Fiyat: " + patatoPrice + " - Gramaj: " + patatoGram + " - Toplam Tutar: " + potatoTotalPrice);
+            //Console.WriteLine("Alınan Ürün: Domates - " + "Birim Fiyat: " + tomatoPrice + " - Gramaj: " + tomatoGram + " - Toplam Tutar: " + tomatoTotalPrice);
+
+            //double shoppingTotalPrice = appleTotalPrice + orangeTotalPrice + strawberryTotalPrice + potatoTotalPrice + tomatoTotalPrice;
+            //Console.WriteLine();
+            //Console.WriteLine();
+
+            //Console.WriteLine("Alışveriş Toplam Tutar: " + shoppingTotalPrice + " TL");
+            #endregion
+
+            #region CharVariables
+            char symbol = 'A';
+            Console.WriteLine(symbol);
+
+            #endregion
+
+            #region Klavyeden Veri Girişleri
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
